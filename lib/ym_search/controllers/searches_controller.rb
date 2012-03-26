4c4c48f467fe07bd6ccbfb47a6006e9f98e3e89d
Matt Atkins <matt@yoomee.com>
2012-03-26 16:45:14 +0100

    Deal with blank params, all of result is link.

--- lib/ym_search/controllers/searches_controller.rb
+++ lib/ym_search/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ include ActionView::Helpers::SanitizeHelper
 module YmSearch::SearchesController
     
   def show
-    @query = strip_tags(params[:q]).strip
+    @query = strip_tags(params[:q]).to_s.strip
     if @query.present?
       @results = ThinkingSphinx.search(@query,:page => params[:page], :per_page => 20)
     else
